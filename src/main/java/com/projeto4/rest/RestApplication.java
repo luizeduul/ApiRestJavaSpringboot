43f194422301981a3814ac99896177b5b9e38e2c
Luiz Uliana <luizeduardr@gmail.com>
2021-07-30 11:16:13 -0300

    Configure wildfly to deploy project

--- src/main/java/com/projeto4/rest/RestApplication.java
+++ src/main/java/com/projeto4/rest/RestApplication.java
@@ -2,12 +2,19 @@ package com.projeto4.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestApplication {
+public class RestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(getClass());
+	}
 }
